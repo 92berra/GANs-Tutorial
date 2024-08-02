f0f96ef630e6050ad9c04faecd017f88d9df5bd5
92berra <ohyunwha@gmail.com>
2024-08-02 14:03:43 +0900

    Add: DCGAN initiate

---
+++ Lab2_DCGAN/DCGAN.py
@@ -0,0 +1,31 @@
+#%matplotlib inline
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+
+# MPS
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+print(f"{device} is available.")
+
+
+# 코드 실행결과의 동일성을 위해 무작위 시드를 설정합니다
+manualSeed = 999
+#manualSeed = random.randint(1, 10000) # 만일 새로운 결과를 원한다면 주석을 없애면 됩니다
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+torch.use_deterministic_algorithms(True) # 결과 재현을 위해 필요합니다
