bce267a58eafb4040c5c68186a240c0e6f409baf
92berra <ohyunwha@gmail.com>
2024-08-07 16:59:18 +0900

    Edit DCGAN

--- Lab2_DCGAN/DCGAN.py
+++ Lab2_DCGAN/DCGAN.py
@@ -1,90 +1,187 @@
-#%matplotlib inline
-import random
 import torch
-import torch.nn as nn
-import torch.nn.parallel
-import torch.optim as optim
 import torch.utils.data
+import torch.nn.parallel
+import torch.nn as nn
+import torch.optim as optim
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 import numpy as np
+import os, argparse
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from IPython.display import HTML
-
-manualSeed = 999
-print("Random Seed: ", manualSeed)
-random.seed(manualSeed)
-torch.manual_seed(manualSeed)
-torch.use_deterministic_algorithms(True) 
 
 
-# 데이터셋의 경로
-dataroot = "../datasets/celeba"
 
-# dataloader에서 사용할 쓰레드 수
-workers = 2
+# MPS
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+print(f"{device} is available.")
 
-# 배치 크기
-batch_size = 128
 
-# 이미지의 크기입니다. 모든 이미지를 변환하여 64로 크기가 통일됩니다.
-image_size = 64
+# Default path
+DEFAULT_DATASET_DIR = '../datasets/celeba'
+DEFAULT_MODEL_DIR = '../result/Lab2/model'
+DEFAULT_IMAGES_DIR = '../result/Lab2/images'
+DEFAULT_LOSS_DIR = '../result/Lab2/loss'
 
-# 이미지의 채널 수로, RGB 이미지이기 때문에 3으로 설정합니다.
-nc = 3
 
-# 잠재공간 벡터의 크기 (예. 생성자의 입력값 크기)
-nz = 100
+# Hyperparameters
+WORKERS = 2                     # Number of thread for dataloader
+BATCH_SIZE = 128                # Batch size
+IMAGE_SIZE = 64                 # Image size
+NC = 3                          # Number of Channel, RGB is 3
+NZ = 100                        # Size of latency space(input of generator)
+NGF = 64                        # Size of feature data pass through G
+NDF = 64                        # Size of feature data pass through D
+EPOCHS = 5                      # Number of epoch
+LEARNING_RATE = 0.0002          # Learning rate of optimizer 
+BETA1 = 0.5                     # beta1 Hypterparameter of Adam optimizer
+NGPU = 1                        # Number of GPU. If cpu, text 0.
 
-# 생성자를 통과하는 특징 데이터들의 채널 크기
-ngf = 64
 
-# 구분자를 통과하는 특징 데이터들의 채널 크기
-ndf = 64
+# Generator
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # 입력데이터 Z가 가장 처음 통과하는 전치 합성곱 계층입니다.
+            nn.ConvTranspose2d( NZ, NGF * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(NGF * 8),
+            nn.ReLU(True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ngf*8) x 4 x 4``
+            nn.ConvTranspose2d(NGF * 8, NGF * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(NGF * 4),
+            nn.ReLU(True),
+            # 위의 계층을 통과한 데이터의 크기. ``(NGF*4) x 8 x 8``
+            nn.ConvTranspose2d( NGF * 4, NGF * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(NGF * 2),
+            nn.ReLU(True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ngf*2) x 16 x 16``
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ngf) x 32 x 32``
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # 위의 계층을 통과한 데이터의 크기. ``(nc) x 64 x 64``
+        )
+    def forward(self, input):
+        return self.main(input)
+    
+    
+# Discriminator 
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # 입력 데이터의 크기는 ``(nc) x 64 x 64`` 입니다
+            nn.Conv2d(nc, NDF, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 위의 계층을 통과한 데이터의 크기. ``(NDF) x 32 x 32``
+            nn.Conv2d(NDF, NDF * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(NDF * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 위의 계층을 통과한 데이터의 크기. ``(NDF*2) x 16 x 16``
+            nn.Conv2d(NDF * 2, NDF * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(NDF * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 위의 계층을 통과한 데이터의 크기. ``(NDF*4) x 8 x 8``
+            nn.Conv2d(NDF * 4, NDF * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(NDF * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 위의 계층을 통과한 데이터의 크기. ``(NDF*8) x 4 x 4``
+            nn.Conv2d(NDF * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+    def forward(self, input):
+        return self.main(input)
 
-# 학습할 에폭 수
-num_epochs = 5
+# Visualize
+def visualize_training(epoch, d_losses, g_losses):
+    print(f'epoch: {epoch}, Discriminator Loss: {np.asarray(d_losses).mean():.4f}, Generator Loss: {np.asarray(g_losses).mean():.4f}')
+    
 
-# 옵티마이저의 학습률
-lr = 0.0002
 
-# Adam 옵티마이저의 beta1 하이퍼파라미터
-beta1 = 0.5
 
-# 사용가능한 gpu 번호. CPU를 사용해야 하는경우 0으로 설정하세요
-ngpu = 1
-
-if __name__ == '__main__':
-    # 우리가 설정한 대로 이미지 데이터셋을 불러와 봅시다
-    # 먼저 데이터셋을 만듭니다
-    dataset = dset.ImageFolder(root=dataroot,
-                            transform=transforms.Compose([
-                                transforms.Resize(image_size),
-                                transforms.CenterCrop(image_size),
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                            ]))
-    # dataloader를 정의해봅시다
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+def train(dataset_dir, workers, batch_size, image_size, nc, nz, ngf, ndf, epochs, learning_rate, beta1, ngpu, model_dir, images_dir, loss_dir):
+    
+    training_data = dset.ImageFolder(
+        root=dataset_dir,
+        transform=transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.CenterCrop(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]))
+    train_loader = torch.utils.data.DataLoader(training_data, batch_size=batch_size,
                                             shuffle=True, num_workers=workers)
 
-    # GPU 사용여부를 결정해 줍니다
-    #device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
-    device = torch.device("mps" if (torch.backends.mps.is_available() and ngpu > 0) else "cpu")
-    print(f"{device} is available.")
 
-    # 학습 데이터들 중 몇가지 이미지들을 화면에 띄워봅시다
-    # real_batch = next(iter(dataloader))
-    # plt.figure(figsize=(8,8))
-    # plt.axis("off")
-    # plt.title("Training Images")
-    # plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-    # plt.show()
+    # Generate save path
+    model_dir = os.path.join(model_dir)
+    os.makedirs(model_dir, exist_ok=True)
 
+    image_dir = os.path.join(images_dir)
+    os.makedirs(image_dir, exist_ok=True)
 
-    # ``netG`` 와 ``netD`` 에 적용시킬 커스텀 가중치 초기화 함수
+    loss_dir = os.path.join(loss_dir)
+    os.makedirs(loss_dir, exist_ok=True)
+    
+    # Save
+    def save_loss(epoch, d_losses, g_losses, loss_dir):
+        os.makedirs(loss_dir, exist_ok=True)
+
+        plt.figure(figsize=(8, 4))
+        plt.plot(d_losses, label='Discriminator Loss')
+        plt.plot(g_losses, label='Generatror Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.title(f'epoch: {epoch}, Discriminator Loss: {np.asarray(d_losses).mean():.4f}, Generator Loss: {np.asarray(g_losses).mean():.4f}')
+        plt.savefig(os.path.join(loss_dir, f'generated_images_epoch_{epoch}.png'))
+        plt.close()    
+
+    def save_sample(epoch, image_dir, NOISE):
+        noise = torch.randn(24, NOISE).to(device)
+        generator.eval()
+        with torch.no_grad():
+            generated_images = generator(noise).cpu().detach().numpy()
+        generated_images = generated_images.reshape(-1, 28, 28) * 255
+        generated_images = generated_images.astype(np.uint8)
+        
+        plt.figure(figsize=(8, 4))
+        for i in range(generated_images.shape[0]):
+            plt.subplot(4, 6, i+1)
+            plt.imshow(generated_images[i], interpolation='nearest', cmap='gray')
+            plt.axis('off')
+        plt.tight_layout()
+        plt.savefig(os.path.join(image_dir, f'generated_images_epoch_{epoch}.png'))
+        plt.close()
+        
+    
+    # Create Generator
+    netG = Generator(ngpu).to(device)
+
+    # If you need multi-gpu, set below
+    if (device.type == 'cuda') and (ngpu > 1):
+        netG = nn.DataParallel(netG, list(range(ngpu)))
+        
+    netG.apply(weights_init) # Initialize (mean=0, stdev=0.02)
+    print(f'Generator: {netG}') # Check Generator Architecture
+    
+    
+    # Create Discriminator
+    netD = Discriminator(ngpu).to(device)
+
+    # If you need multi-gpu, set below
+    if (device.type == 'cuda') and (ngpu > 1):
+        netD = nn.DataParallel(netD, list(range(ngpu)))
+        
+    netD.apply(weights_init) # Initialize (mean=0, stdev=0.02)
+    print(netD) # Check Generator Architecture
+    
+    # Customize weight initialization function (apply to netG, netD)
     def weights_init(m):
         classname = m.__class__.__name__
         if classname.find('Conv') != -1:
@@ -93,131 +190,35 @@ if __name__ == '__main__':
             nn.init.normal_(m.weight.data, 1.0, 0.02)
             nn.init.constant_(m.bias.data, 0)
             
-            
-    # 생성자 코드
-
-    class Generator(nn.Module):
-        def __init__(self, ngpu):
-            super(Generator, self).__init__()
-            self.ngpu = ngpu
-            self.main = nn.Sequential(
-                # 입력데이터 Z가 가장 처음 통과하는 전치 합성곱 계층입니다.
-                nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
-                nn.BatchNorm2d(ngf * 8),
-                nn.ReLU(True),
-                # 위의 계층을 통과한 데이터의 크기. ``(ngf*8) x 4 x 4``
-                nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(ngf * 4),
-                nn.ReLU(True),
-                # 위의 계층을 통과한 데이터의 크기. ``(ngf*4) x 8 x 8``
-                nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(ngf * 2),
-                nn.ReLU(True),
-                # 위의 계층을 통과한 데이터의 크기. ``(ngf*2) x 16 x 16``
-                nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(ngf),
-                nn.ReLU(True),
-                # 위의 계층을 통과한 데이터의 크기. ``(ngf) x 32 x 32``
-                nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
-                nn.Tanh()
-                # 위의 계층을 통과한 데이터의 크기. ``(nc) x 64 x 64``
-            )
-
-        def forward(self, input):
-            return self.main(input)
-        
-        
-    # 생성자를 만듭니다
-    netG = Generator(ngpu).to(device)
-
-    # 필요한 경우 multi-GPU를 설정 해주세요
-    if (device.type == 'cuda') and (ngpu > 1):
-        netG = nn.DataParallel(netG, list(range(ngpu)))
-
-    # 모든 가중치의 평균을 0( ``mean=0`` ), 분산을 0.02( ``stdev=0.02`` )로 초기화하기 위해
-    # ``weight_init`` 함수를 적용시킵니다
-    netG.apply(weights_init)
-
-    # 모델의 구조를 출력합니다
-    print(netG)
-
-
-    # 구분자 코드
-
-    class Discriminator(nn.Module):
-        def __init__(self, ngpu):
-            super(Discriminator, self).__init__()
-            self.ngpu = ngpu
-            self.main = nn.Sequential(
-                # 입력 데이터의 크기는 ``(nc) x 64 x 64`` 입니다
-                nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-                nn.LeakyReLU(0.2, inplace=True),
-                # 위의 계층을 통과한 데이터의 크기. ``(ndf) x 32 x 32``
-                nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(ndf * 2),
-                nn.LeakyReLU(0.2, inplace=True),
-                # 위의 계층을 통과한 데이터의 크기. ``(ndf*2) x 16 x 16``
-                nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(ndf * 4),
-                nn.LeakyReLU(0.2, inplace=True),
-                # 위의 계층을 통과한 데이터의 크기. ``(ndf*4) x 8 x 8``
-                nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(ndf * 8),
-                nn.LeakyReLU(0.2, inplace=True),
-                # 위의 계층을 통과한 데이터의 크기. ``(ndf*8) x 4 x 4``
-                nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-                nn.Sigmoid()
-            )
-
-        def forward(self, input):
-            return self.main(input)
-        
-        
-    # 구분자를 만듭니다
-    netD = Discriminator(ngpu).to(device)
-
-    # 필요한 경우 multi-GPU를 설정 해주세요
-    if (device.type == 'cuda') and (ngpu > 1):
-        netD = nn.DataParallel(netD, list(range(ngpu)))
-
-    # 모든 가중치의 평균을 0( ``mean=0`` ), 분산을 0.02( ``stdev=0.02`` )로 초기화하기 위해
-    # ``weight_init`` 함수를 적용시킵니다
-    netD.apply(weights_init)
-
-    # 모델의 구조를 출력합니다
-    print(netD)
-
-
-
-
-    # ``BCELoss`` 함수의 인스턴스를 초기화합니다
+    
+    # Initialize BCELoss
     criterion = nn.BCELoss()
 
-    # 생성자의 학습상태를 확인할 잠재 공간 벡터를 생성합니다
+    # Create latency space vector to check G 
     fixed_noise = torch.randn(64, nz, 1, 1, device=device)
 
-    # 학습에 사용되는 참/거짓의 라벨을 정합니다
+    # Fix True/False label for training
     real_label = 1.
     fake_label = 0.
 
-    # G와 D에서 사용할 Adam옵티마이저를 생성합니다
+    # Create Adam Optimizer for G, D
     optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
     optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
 
-
-    # 학습 과정
-
-    # 학습상태를 체크하기 위해 손실값들을 저장합니다
+    # Save loss value for training
     img_list = []
     G_losses = []
     D_losses = []
     iters = 0
 
     print("Starting Training Loop...")
-    # 에폭(epoch) 반복
-    for epoch in range(num_epochs):
-        # 한 에폭 내에서 배치 반복
-        for i, data in enumerate(dataloader, 0):
+    
+    
+    # Iterate
+    for epoch in range(EPOCHS):
+        
+        # Batch iteration per epoch
+        for i, data in enumerate(train_loader, 0):
 
             ############################
             # (1) D 신경망을 업데이트 합니다: log(D(x)) + log(1 - D(G(z)))를 최대화 합니다
@@ -272,20 +273,45 @@ if __name__ == '__main__':
             # G를 업데이트 합니다
             optimizerG.step()
 
-            # 훈련 상태를 출력합니다
+            # Print training result
             if i % 50 == 0:
                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                    % (epoch, num_epochs, i, len(dataloader),
+                    % (epoch, EPOCHS, i, len(train_loader),
                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
-            # 이후 그래프를 그리기 위해 손실값들을 저장해둡니다
+            # Save loss value for graph
             G_losses.append(errG.item())
             D_losses.append(errD.item())
 
-            # fixed_noise를 통과시킨 G의 출력값을 저장해둡니다
-            if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+            # Save output of G passed fixed_noise
+            if (iters % 500 == 0) or ((epoch == EPOCHS-1) and (i == len(train_loader)-1)):
                 with torch.no_grad():
                     fake = netG(fixed_noise).detach().cpu()
                 img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
             iters += 1
+      
+          
+# Main
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--dataset_dir', type=str, dest='dataset_dir', default=DEFAULT_DATASET_DIR)
+    parser.add_argument('--worker', type=str, dest='worker', default=WORKERS)
+    parser.add_argument('--batch_size', type=str, dest='batch_size', default=BATCH_SIZE)
+    parser.add_argument('--image_size', type=str, dest='image_size', default=IMAGE_SIZE)
+    parser.add_argument('--nc', type=str, dest='nc', default=NC)
+    parser.add_argument('--nz', type=str, dest='nz', default=NZ)
+    parser.add_argument('--ngf', type=str, dest='ngf', default=NGF)
+    parser.add_argument('--ndf', type=str, dest='ndf', default=NDF)
+    parser.add_argument('--epochs', type=str, dest='epochs', default=EPOCHS)
+    parser.add_argument('--learning_rate', type=str, dest='learning_rate', default=LEARNING_RATE)
+    parser.add_argument('--beta1', type=str, dest='beta1', default=BETA1)
+    parser.add_argument('--ngpu', type=str, dest='ngpu', default=NGPU)
+    parser.add_argument('--model_dir', type=str, dest='model_dir', default=DEFAULT_MODEL_DIR)
+    parser.add_argument('--images_dir', type=str, dest='images_dir', default=DEFAULT_IMAGES_DIR)
+    parser.add_argument('--loss_dir', type=str, dest='loss_dir', default=DEFAULT_LOSS_DIR)
+
+    args = parser.parse_args()
+    
+    train(args.dataset_dir, args.workers, args.batch_size, args.image_size, args.nc, args.nz, args.ngf, args.ndf, args.epochs, args.learning_rate, args.beta1, args.ngpu, args.model_dir, args.images_dir, args.loss_dir)
